3cee5d8eda46f8525766de85be8665573db971c8
SuvankarD-Vyoma <suvankar.d@vyomainnovusglobal.com>
2025-11-28 16:18:55 +0530

    done

--- .next/dev/static/chunks/app_candidates_page_tsx_618d4f32._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_29459b7f._.js",
-  "static/chunks/node_modules_lucide-react_dist_esm_icons_c6eb0a4e._.js"
-],
-    source: "dynamic"
-});
